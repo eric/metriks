9e68b99aca758d0e74788486579b38ad61746481
Aphyr <aphyr@aphyr.com>
2012-03-11 17:21:13 -0700

    Add tests for thread-safe meters, histograms, and counters.

--- test/counter_test.rb
+++ test/counter_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 require 'metriks/counter'
 
 class CounterTest < Test::Unit::TestCase
+  include ThreadHelper
+
   def setup
     @counter = Metriks::Counter.new
   end
@@ -13,9 +15,25 @@ class CounterTest < Test::Unit::TestCase
     assert_equal 1, @counter.count
   end
 
+  def test_increment_threaded
+    thread 10, n: 100 do
+      @counter.increment
+    end
+
+    assert_equal 1000, @counter.count
+  end
+
   def test_increment_by_more
     @counter.increment 10
 
     assert_equal 10, @counter.count
+  end
+  
+  def test_increment_by_more_threaded
+    thread 10, n: 100 do
+      @counter.increment 10
+    end
+
+    assert_equal 10000, @counter.count
   end
 end
