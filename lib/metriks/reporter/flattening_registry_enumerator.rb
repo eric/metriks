f6c6de4b397a7accc5b1de5965f3fe4904f2be74
Eric Lindvall <eric@5stops.com>
2013-04-17 00:32:30 -0700

    Look at combining the counter tracking

--- lib/metriks/reporter/flattening_registry_enumerator.rb
+++ lib/metriks/reporter/flattening_registry_enumerator.rb
@@ -1,29 +1,43 @@
 module Metriks
   module Reporter
     class FlatteningRegistryEnumerator
-      def initialize(registry)
+      def initialize(registry, options = {})
         @registry = registry
+        if options[:counter_derivitive]
+          @last = Hash.new { |h,k| h[k] = 0 }
+        end
       end
       
       def each(&block)
         @registry.each do |name, metric|
           case metric
           when Metriks::Meter
-            block.call("#{name}.count", metric.count)
-            block.call("#{name}.one_minute_rate", metric.one_minute_rate)
-            block.call("#{name}.five_minute_rate", metric.five_minute_rate)
-            block.call("#{name}.fifteen_minute_rate", metric.fifteen_minute_rate)
-            block.call("#{name}.mean_rate", metric.mean_rate)
+            if @last
+              count = metric.count
+              block.call("#{name}.count", count - @last["#{name}.count"])
+              @last["#{name}.count"] = count
+            else
+              block.call("#{name}.count", metric.count)
+              block.call("#{name}.one_minute_rate", metric.one_minute_rate)
+              block.call("#{name}.five_minute_rate", metric.five_minute_rate)
+              block.call("#{name}.fifteen_minute_rate", metric.fifteen_minute_rate)
+              block.call("#{name}.mean_rate", metric.mean_rate)
+            end
+
           when Metriks::Counter
             block.call("#{name}.count", metric.count)
           when Metriks::Gauge
             block.call("#{name}.value", metric.value)
           when Metriks::UtilizationTimer
-            block.call("#{name}.count", metric.count)
-            block.call("#{name}.one_minute_rate", metric.one_minute_rate)
-            block.call("#{name}.five_minute_rate", metric.five_minute_rate)
-            block.call("#{name}.fifteen_minute_rate", metric.fifteen_minute_rate)
-            block.call("#{name}.mean_rate", metric.mean_rate)
+            if @last
+              count = metric.count
+              block.call("#{name}.count", count - @last["#{name}.count"])
+              @last["#{name}.count"] = count
+            else
+              block.call("#{name}.one_minute_rate", metric.one_minute_rate)
+              block.call("#{name}.five_minute_rate", metric.five_minute_rate)
+              block.call("#{name}.fifteen_minute_rate", metric.fifteen_minute_rate)
+            end
 
             block.call("#{name}.min", metric.min)
             block.call("#{name}.max", metric.max)
@@ -43,11 +57,17 @@ module Metriks
             block.call("#{name}.99th_percentile", snapshot.get_99th_percentile)
             block.call("#{name}.999th_percentile", snapshot.get_999th_percentile)
           when Metriks::Timer
-            block.call("#{name}.count", metric.count)
-            block.call("#{name}.one_minute_rate", metric.one_minute_rate)
-            block.call("#{name}.five_minute_rate", metric.five_minute_rate)
-            block.call("#{name}.fifteen_minute_rate", metric.fifteen_minute_rate)
-            block.call("#{name}.mean_rate", metric.mean_rate)
+            if @last
+              count = metric.count
+              block.call("#{name}.count", count - @last["#{name}.count"])
+              @last["#{name}.count"] = count
+            else
+              block.call("#{name}.count", metric.count)
+              block.call("#{name}.one_minute_rate", metric.one_minute_rate)
+              block.call("#{name}.five_minute_rate", metric.five_minute_rate)
+              block.call("#{name}.fifteen_minute_rate", metric.fifteen_minute_rate)
+              block.call("#{name}.mean_rate", metric.mean_rate)
+            end
 
             block.call("#{name}.min", metric.min)
             block.call("#{name}.max", metric.max)
